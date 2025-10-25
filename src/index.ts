b17854bf88e4ddb7dca72922727e30ed0b762814
Silvan Gehrig <38138071+Sillyvan@users.noreply.github.com>
2025-10-25 13:31:12 +0200

    hono lib

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { MyAgent } from "./agent";
+import { agentsMiddleware } from "hono-agents";
 
 export { MyAgent };
 
 const app = new Hono();
+app.use("*", agentsMiddleware());
 
 app.get("/", (c) => {
   return c.text("Hello World!");
